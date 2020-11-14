7ca1bcd780d3d4b9273287de58de9cf747bb0a9d
Gabriela Zalta <gzalta96@gmail.com>
2020-11-14 16:40:33 -0500

    working on local storage

--- js/script.js
+++ js/script.js
@@ -5,14 +5,26 @@ $("#currentDay").html(currentDate);
 $(document).ready(function(){
     $(".saveBtn").on("click", function(){
 
-        var task = $(this).val(".description");
-        var time = $(this).val("#hour")
+        var task = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
         //when user click save, info is saved into localStorage
         localStorage.setItem(task, time);
+
+        //keep information after user refrshes page
+        var taskSaved = localStorage.getItem(task, time);
+        $(this).val(taskSaved);
+        
     })
-})
+
+
+
 //make past times grey, future green, present red
+
+
+})
+
+
 
 
 
